d9c36b9c96e85c6db2e428cd25f35d2e7f594818
V Play Games <vaibhavnargwani28@gmail.com>
2025-07-05 15:50:28 +0530

    Add filtering

--- src/main/kotlin/Routing.kt
+++ src/main/kotlin/Routing.kt
@@ -8,8 +8,24 @@ import net.vpg.vjson.value.JSONArray.Companion.toJSON
 fun Application.configureRouting() {
     routing {
         get("/") {
+            val params = call.request.queryParameters
+            val categories = params["category"]
+                ?.split(",")
+                ?.map { it.lowercase() }
+                ?.distinct()
+            val years = params["year"]
+                ?.split(",")
+                ?.mapNotNull { it.toIntOrNull() }
+                ?.distinct()
+
+            val filtered = MovieRepository.movies
+                .filter { categories == null || categories.isEmpty() || categories.contains(it.category) }
+                .filter { years == null || years.isEmpty() || years.contains(it.year) }
             call.respondText(
-                MovieRepository.movies.random().toObject().toPrettyString()
+                if (filtered.isEmpty())
+                    "null"
+                else
+                    filtered.random().toObject().toPrettyString()
             )
         }
         get("/years") {
